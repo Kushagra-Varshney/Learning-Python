e731b969eb7dd2707bb659b5f7975a44bad2a6c1
Kushagra-Varshney <kushagra.ashishvarshney@gmail.com>
2024-07-18 17:12:38 +0530

    Loops

---
+++ loops/for.py
@@ -0,0 +1,11 @@
+for i in range(4):
+    print(i)
+# Output: 0 1 2 3 
+
+for i in range(1, 5, 2):
+    print(i)
+
+#print list using for loop
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+    print(x)
---
+++ loops/while.py
@@ -0,0 +1,14 @@
+
+i = 1
+
+while(i < 6):
+    print(i)
+    i += 1
+
+#print a list using while loop
+fruits = ["apple", "banana", "cherry"]
+i = 0
+while i < len(fruits):
+    print(fruits[i])
+    i += 1
+    
