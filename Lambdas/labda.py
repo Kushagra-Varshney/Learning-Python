290dbb87580181e5dd022d464c35b98e728b07e3
Kushagra-Varshney <kushagra.ashishvarshney@gmail.com>
2024-07-19 15:47:35 +0530

    Complete

---
+++ Lambdas/labda.py
@@ -0,0 +1,6 @@
+# Define a lambda function that adds two numbers
+add_numbers = lambda x, y: x + y
+
+# Use the lambda function to add two numbers
+result = add_numbers(5, 3)
+print(result)  # Output: 8
